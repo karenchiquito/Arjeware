6aab6273cd60f0269685b12d6057d1c0ad5b5120
Jhon Sebastian <sebastamriel@gmail.com>
2018-09-22 23:29:08 -0500

    Se agrego el DAO interface de tarifa energía

---
+++ src/co/com/Arjeware/Arjeware/dao/TarifaEnergiaDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.Arjeware.Arjeware.dao;
+
+import co.com.Arjeware.Arjeware.entity.TarifaEnergiaEntity;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sebas
+ */
+public interface TarifaEnergiaDAO {
+    
+    public ArrayList<TarifaEnergiaEntity> obtenerTarifas();
+    
+}
